d072c5882a71b4e7d9d823bc5e02e32fa88a4540
advorange <advorange@live.com>
2016-12-13 23:00:34 -0800

    Update Program.cs

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -12,15 +12,25 @@ namespace Advobot
 		public static void Main(string[] args) =>
 			new Program().Start().GetAwaiter().GetResult();
 
-		private DiscordSocketClient client;
-		private CommandHandler handler;
+		//public DiscordSocketClient Client { get { return client; } }
 
 		public async Task Start()
 		{
 			//Define the DiscordSocketClient
+			DiscordSocketClient client;
 			client = new DiscordSocketClient(new DiscordSocketConfig());
 
 			client.Log += Log;
+			client.GuildAvailable += BotLogs.OnGuildAvailable;
+			client.JoinedGuild += BotLogs.OnJoinedGuild;
+			client.LeftGuild += BotLogs.OnLeftGuild;
+			client.UserJoined += ServerLogs.OnUserJoined;
+			client.UserLeft += ServerLogs.OnUserLeft;
+			client.GuildMemberUpdated += ServerLogs.OnGuildMemberUpdated;
+			client.UserBanned += ServerLogs.OnUserBanned;
+			client.UserUnbanned += ServerLogs.OnUserUnbanned;
+			client.MessageUpdated += ServerLogs.OnMessageUpdated;
+			client.MessageDeleted += ServerLogs.OnMessageDeleted;
 
 			//Login and connect to Discord.
 			await client.LoginAsync(TokenType.Bot, "Key");
@@ -29,6 +39,7 @@ namespace Advobot
 			var map = new DependencyMap();
 			map.Add(client);
 
+			CommandHandler handler;
 			handler = new CommandHandler();
 			await handler.Install(map);
 
