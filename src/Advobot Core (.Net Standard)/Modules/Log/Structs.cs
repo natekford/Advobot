3749b4d2400f3925351335232581abbedbbb9990
Advorange <advorange@live.com>
2017-09-27 15:03:55 -0700

    Log files weren'tuploaded, idk why.

---
+++ src/Advobot Core (.Net Standard)/Modules/Log/Structs.cs
@@ -0,0 +1,17 @@
+﻿using Advobot.Interfaces;
+using Discord;
+
+namespace Advobot.Modules.Log
+{
+	internal struct VerifiedLoggingAction
+	{
+		public IGuild Guild { get; }
+		public IGuildSettings GuildSettings { get; }
+
+		public VerifiedLoggingAction(IGuild guild, IGuildSettings guildSettings)
+		{
+			Guild = guild;
+			GuildSettings = guildSettings;
+		}
+	}
+}
