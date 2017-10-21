847904c719fce1e79efbca55d764220f9dd3e61a
Advorange <advorange@live.com>
2017-10-20 20:41:01 -0700

    WHY DO THESE FILES ALWAYS GET IGNORED?

---
+++ src/Advobot.Core/Services/Log/Loggers/Logger.cs
@@ -0,0 +1,27 @@
+﻿using Advobot.Core.Actions;
+using Advobot.Core.Enums;
+using Advobot.Core.Interfaces;
+using Discord;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Advobot.Core.Services.Log.Loggers
+{
+	public abstract class Logger
+	{
+		protected ILogService _Logging;
+		protected IDiscordClient _Client;
+		protected IBotSettings _BotSettings;
+		protected IGuildSettingsService _GuildSettings;
+		protected ITimersService _Timers;
+
+		public Logger(ILogService logging, IServiceProvider provider)
+		{
+			_Logging = logging;
+			_Client = provider.GetRequiredService<IDiscordClient>();
+			_BotSettings = provider.GetRequiredService<IBotSettings>();
+			_GuildSettings = provider.GetRequiredService<IGuildSettingsService>();
+			_Timers = provider.GetRequiredService<ITimersService>();
+		}
+	}
+}
