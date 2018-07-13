fdf17deb5abc9ad7603c8f0a5616a9b528fe8262
advorange <advorange@live.com>
2018-07-12 21:31:06 -0700

    Not sure why it ignored the logging file again.

---
+++ src/Advobot/Services/Log/Loggers/BotLogger.cs
@@ -0,0 +1,27 @@
+﻿using Advobot.Interfaces;
+using AdvorangesUtils;
+using Discord;
+using System;
+using System.Threading.Tasks;
+
+namespace Advobot.Services.Log.Loggers
+{
+	internal sealed class BotLogger : Logger, IBotLogger
+	{
+		internal BotLogger(ILogService logging, IServiceProvider provider) : base(logging, provider) { }
+
+		/// <summary>
+		/// Logs system messages from the Discord .Net library.
+		/// </summary>
+		/// <param name="message"></param>
+		/// <returns></returns>
+		public Task OnLogMessageSent(LogMessage message)
+		{
+			if (!String.IsNullOrWhiteSpace(message.Message))
+			{
+				ConsoleUtils.WriteLine(message.Message, name: message.Source);
+			}
+			return Task.CompletedTask;
+		}
+	}
+}
