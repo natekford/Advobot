7da146384be554981381c2075fb2bc144a3793dc
Advorange <advorange@live.com>
2019-10-04 20:56:27 -0700

    Made a meta project grouping all the commands together.

---
+++ src/Commands/Advobot.Logging/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+using Advobot;
+using Advobot.CommandAssemblies;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("Advorange")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyDescription("Logging for certain events in Discord.")]
+[assembly: AssemblyProduct("Advobot")]
+[assembly: AssemblyTitle("Advobot.Logging")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5f35e0b0-1196-4eb2-8d64-86755f0fcde4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion(Constants.BOT_VERSION)]
+[assembly: AssemblyFileVersion(Constants.BOT_VERSION)]
+[assembly: AssemblyInformationalVersion(Constants.BOT_VERSION)]
+
+// Indicates the assembly has commands in it for the bot to use
+[assembly: CommandAssembly("en-US")]
+[assembly: InternalsVisibleTo("Advobot.Tests")]
