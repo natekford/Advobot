ae63717b38f36556c7161064184ab0d19928e1f4
Advorange <advorange@live.com>
2019-10-22 23:14:29 -0700

    Fixed not compiling.

--- src/Advobot.Core/AssemblyVersion.cs
+++ src/Advobot.Core/AssemblyVersion.cs
@@ -6,6 +6,6 @@ namespace Advobot
 	internal static class Version
 	{
 		//To make sure every attribute has the same version number.
-		public const string VERSION_NUMBER = "3.2.7235.11120";
+		public const string VERSION_NUMBER = "3.2.7235.11226";
 	}
 }
