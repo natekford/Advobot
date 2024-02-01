4cf2c22032072da32432318aa00fb059b13a35e2
natekford <nkeelanford@gmail.com>
2024-01-31 22:29:43 -0800

    Tidied csprojs.

--- src/Advobot.Core/Resources/BotSettingNames.Designer.cs
+++ src/Advobot.Core/Resources/BotSettingNames.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Advobot.Resources {
+namespace Advobot.Core.Resources {
     using System;
     
     
@@ -19,7 +19,7 @@ namespace Advobot.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class BotSettingNames {
@@ -39,7 +39,7 @@ namespace Advobot.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Advobot.Resources.BotSettingNames", typeof(BotSettingNames).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Advobot.Core.Resources.BotSettingNames", typeof(BotSettingNames).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
